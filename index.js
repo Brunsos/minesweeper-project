7e68c33dfdcec4eb55ebffed27ae0c3c18cecfa5
Brunsos <zorian.hudym@gmail.com>
2024-06-26 10:07:31 -0600

    18:00
    progress

--- index.js
+++ index.js
@@ -2,7 +2,19 @@
 
 import { createBoard } from "./minesweeper.js"
 
-console.log(createBoard(2,2))
+const BOARD_SIZE = 10
+const NUMBER_OF_MINES = 2
+
+const board = (createBoard(BOARD_SIZE,NUMBER_OF_MINES))
+const boardElement = document.querySelector('.board')
+
+board.forEach(row => {
+  row.forEach(tile => {
+    boardElement.append(tile.element)
+  })
+})
+boardElement.style.setProperty("--size",BOARD_SIZE)
+
 /*
 1. fill board with tiles and mines
 2.left click listener
